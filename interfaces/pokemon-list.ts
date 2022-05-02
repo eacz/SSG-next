ca41d364b22a803d787dfd09720ce02f511609b9
Esteban Canteros <estebanacanteros@gmail.com>
2022-05-02 15:11:51 -0300

    displaying pokemon data through GetStaticProps

---
+++ interfaces/pokemon-list.ts
@@ -0,0 +1,13 @@
+export interface PokemonListResponse {
+  count: number
+  next?: string
+  previous?: string
+  results: PokemonItem[]
+}
+
+export interface PokemonItem {
+  name: string
+  url: string
+  id: number
+  img: string
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,23 @@ import { GetStaticProps } from 'next'
 import { NextPage } from 'next'
 import { pokeApi } from '../api'
 import { Layout } from '../components/layouts'
+import { PokemonItem, PokemonListResponse } from '../interfaces'
 
-const HomePage: NextPage = (props) => {
-  console.log({ props })
+interface Props {
+  pokemons: PokemonItem[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  console.log(pokemons)
+
   return (
     <Layout title='Pokemon List'>
       <ul>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
-        <li>Poke</li>
+        {pokemons.map((poke) => (
+          <li key={poke.id}>
+            #{poke.id} - {poke.name}
+          </li>
+        ))}
       </ul>
     </Layout>
   )
@@ -28,11 +26,16 @@ const HomePage: NextPage = (props) => {
 
 //This only runs server-side, and the props are passed to the component
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get('/pokemon?limit=151')
-
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+  //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
+  const pokemons: PokemonItem[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`,
+  }))
   return {
     props: {
-      pokemon: data.results,
+      pokemons,
     },
   }
 }
