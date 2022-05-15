cc756151d1c5015fc3db327f07129efbca8d96e5
Esteban Canteros <estebanacanteros@gmail.com>
2022-05-14 22:39:44 -0300

    OG meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,14 +6,19 @@ interface Props {
   title?: string
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin
+
 const Layout = ({ children, title }: PropsWithChildren<Props>) => {
   return (
     <>
       <Head>
         <title>{title || 'Pokemon App'}</title>
         <meta name='author' content='eacz' />
-        <meta name='description' content='information about {poke}' />
-        <meta name='keywords' content='{poke}, pokemon, pokedex' />
+        <meta name='description' content={`information about ${title}`} />
+        <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+        <meta property='og:title' content={`Information about ${title}`} />
+        <meta property='og:description' content={`This page contains information about ${title}`} />
+        <meta property='og:image' content={`${origin}/images/banner.png`} />
       </Head>
       <Navbar />
       <main style={{ padding: '0px 20px' }}>{children}</main>
